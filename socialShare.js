d9313aee2dcb6709293587edbf881fe85116363a
KristinaHorbenko <129623556+KristinaHorbenko@users.noreply.github.com>
2024-10-28 22:39:14 +0200

    Update socialShare.js

--- socialShare.js
+++ socialShare.js
@@ -17,28 +17,3 @@ document.getElementById('shareArrow').addEventListener('click', function() {
      // Изменить цвет иконки
   iconImage.classList.toggle('white');
 });
-
-// document.getElementById('shareArrow').addEventListener('click', function() {
-//   var icons = document.getElementById('socialIcons');
-//   if (icons.style.display === 'none' || icons.style.display === '') {
-//     icons.style.display = 'flex'; // Показываем иконки
-//   } else {
-//     icons.style.display = 'none'; // Скрываем иконки
-//   }
-// })
-
-// const shareButton = document.getElementById('shareArrow');
-// const icons = document.getElementById('socialIcons');
-// const shareSection = document.querySelector('.share-button');
-
-// shareButton.addEventListener('click', () => {
-//     // Переключаем класс 'active' для раздела кнопки
-//     shareSection.classList.toggle('active');
-    
-//     // Показываем или скрываем иконки
-//     if (icons.style.display === 'none' || icons.style.display === '') {
-//         icons.style.display = 'flex'; // Показываем иконки
-//     } else {
-//         icons.style.display = 'none'; // Скрываем иконки
-//     }
-// });
